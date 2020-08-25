14195be066f94ce6fa5e77cfacc4f6d719048975
neltia <dsz08082@naver.com>
2020-08-25 22:00:02 +0900

    step2

---
+++ step2/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect, url_for
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+class FileForm(FlaskForm):
+	files = FileField(validators=[FileRequired('업로드할 파일을 넣어주세요')])
+
+@app.route('/', methods = ['GET', 'POST'])
+def upload_page():
+    form = FileForm()
+    if form.validate_on_submit():
+        f = form.files.data
+        f.save('./uploads/' + secure_filename(f.filename))
+        return render_template('check.html')
+    return render_template('upload.html', form=form)
+
+if __name__ == '__main__':
+	#서버 실행
+	app.run(host='0.0.0.0', debug = True)
